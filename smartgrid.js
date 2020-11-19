476b576195a00c358072a4018e17b4e41f4108a1
iliagromov <ilia.workproffile@gmail.com>
2020-11-20 02:21:27 +0300

    add project

---
+++ smartgrid.js
@@ -0,0 +1,33 @@
+module.exports = {
+	columns: 12,
+	offset: "20px",
+	//mobileFirst: true,
+	container: {
+        maxWidth: "940px",
+        fields: "60px"
+    },
+	breakPoints: {
+		md: {
+            width: "1024px"
+        },
+        sm: {
+            width: "992px"
+        },
+        ssm: {
+            width: "768px"
+        },
+        xs: {
+            width: "576px",
+            offset: "10px",
+            fields: "20px"
+        },
+        xxs: {
+            width: "360px",
+            /*
+            offset: "10px",
+            fields: "5px"
+            */
+        }
+	},
+    //detailedCalc: true
+};
